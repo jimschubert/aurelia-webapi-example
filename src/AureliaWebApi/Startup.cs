4d80529789bb8898e7462247e10888057ce00d8b
rcalaf <rcalafp@cirsa.com>
2017-10-18 10:09:12 +0200

    Added swagger

--- src/AureliaWebApi/Startup.cs
+++ src/AureliaWebApi/Startup.cs
@@ -50,7 +50,13 @@ namespace AureliaWebApi
                 var db = new BlogContext(services.BuildServiceProvider(), optionsBuilder.Options);
                 
                 services.Add(new ServiceDescriptor(typeof(BlogContext), db));
-                
+
+                // Register the Swagger generator, defining one or more Swagger documents
+                services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                });
+
             } else {
                 // Add framework services.
                 services.AddMvc();
@@ -83,6 +89,15 @@ namespace AureliaWebApi
                 EnableDefaultFiles = true
             };
             app.UseFileServer(staticFileServer);
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseMvc();
             
